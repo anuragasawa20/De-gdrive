3972b5779b8a8537ab83d7d14484cde9d8071312
anuragasawa20 <99682976+anuragasawa20@users.noreply.github.com>
2023-02-04 15:06:17 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
+};
+
+// to run the hardhat node    npx hardhat node
+// to deploy on blockchain     npx hardhat run --network localhost scripts/deploy.js
